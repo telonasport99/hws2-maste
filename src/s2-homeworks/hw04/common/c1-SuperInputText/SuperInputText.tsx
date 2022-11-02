e7afce39075af2656169998e89e18e65afe687d4
telonasport99 <telonasport99@gmail.com>
2022-11-02 22:59:32 +0300

    4-3

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -36,16 +36,16 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChange?.(e) // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
-
-        onChangeText?.(e.currentTarget.value)
+        onChange // если есть пропс onChange, то передать ему е (поскольку onChange не обязателен)
+        && onChange(e)
+        onChangeText && onChangeText(e.currentTarget.value)
     }
     const onKeyPressCallback = (e: KeyboardEvent<HTMLInputElement>) => {
-        onKeyPress?.(e)
+      onKeyPress &&  onKeyPress(e)
 
-        onEnter && // если есть пропс onEnter
-        e.key === 'Enter' && // и если нажата кнопка Enter
-        onEnter() // то вызвать его
+        onEnter  // если есть пропс onEnter
+        && e.key === 'Enter'  // и если нажата кнопка Enter
+        && onEnter() // то вызвать его
     }
 
     const finalSpanClassName = `${s.error} ${spanClassName ? spanClassName : ''}`
