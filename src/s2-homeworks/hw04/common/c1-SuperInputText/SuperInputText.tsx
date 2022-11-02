4820ef6e958be38d12412094527e5484f4115237
telonasport99 <telonasport99@gmail.com>
2022-11-02 19:28:40 +0300

    4

--- src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
+++ src/s2-homeworks/hw04/common/c1-SuperInputText/SuperInputText.tsx
@@ -48,11 +48,8 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
         onEnter() // то вызвать его
     }
 
-    const finalSpanClassName = s.error
-        + (spanClassName ? ' ' + spanClassName : '')
-    const finalInputClassName = s.input
-        + (error ? ' ' + s.errorInput : ' ' + s.superInput)
-        + (className ? ' ' + s.className : '') // задача на смешивание классов
+    const finalSpanClassName = `${s.error} ${spanClassName ? ' ' + spanClassName : ''}`
+    const finalInputClassName = `${s.input} ${error ? s.errorInput : s.superInput} ${className} ` // задача на смешивание классов
 
     return (
         <div className={s.inputWrapper}>
