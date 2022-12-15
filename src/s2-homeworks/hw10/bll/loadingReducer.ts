4b1b1a1ad87be56289c25f7f4ab0bb21fecc0516
telonasport99 <telonasport99@gmail.com>
2022-12-15 22:41:13 +0300

    10

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,18 @@
 const initState = {
     isLoading: false,
 }
+type LoadingStatetype = {
+    isLoading:boolean
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): LoadingStatetype => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING':
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
 
         default:
             return state
