c0a187320caa101c877228e9a37999abd36fe63f
telonasport99 <telonasport99@gmail.com>
2022-12-25 15:13:16 +0300

    11

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -10,6 +10,7 @@ import HW7 from "../s2-homeworks/hw07/HW7";
 import HW8 from "../s2-homeworks/hw08/HW8";
 import HW9 from "../s2-homeworks/hw09/HW9";
 import HW10 from "../s2-homeworks/hw10/HW10";
+import HW11 from "../s2-homeworks/hw11/HW11";
 
 function App() {
     return (
@@ -27,6 +28,7 @@ function App() {
             <HW8/>
             <HW9/>
             <HW10/>
+            <HW11/>
         </div>
     )
 }
