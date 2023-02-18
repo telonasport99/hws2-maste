37a56b6690a900fc1a5a2079293bb231ed762f31
telonasport99 <telonasport99@gmail.com>
2023-02-18 17:08:12 +0300

    13

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -3,10 +3,6 @@ import HW10 from "../../hw10/HW10";
 import HW11 from "../../hw11/HW11";
 import HW12 from "../../hw12/HW12";
 import HW13 from "../../hw13/HW13";
-// import HW10 from '../../hw10/HW10'
-// import HW11 from '../../hw11/HW11'
-// import HW12 from '../../hw12/HW12'
-// import HW13 from '../../hw13/HW13'
 
 function JuniorPlus() {
     return (
